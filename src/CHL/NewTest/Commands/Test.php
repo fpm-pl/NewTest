558f6f5d563c95d0be1da5233b561d092b37687c
FRashkar-C <114340217+FRashkar-pm@users.noreply.github.com>
2022-09-27 21:19:40 +0700

    Create Test.php

---
+++ src/CHL/NewTest/Commands/Test.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CHL\NewTest\Commands;
+
+use CHL\NewTest\Main;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+
+class Test extends Command
