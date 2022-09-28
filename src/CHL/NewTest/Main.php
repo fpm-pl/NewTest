3332b3122f1baff04b635d75c0f088b7e6b12c31
FRashkar-C <114340217+FRashkar-pm@users.noreply.github.com>
2022-09-28 18:30:56 +0700

    Update Main.php

--- src/CHL/NewTest/Main.php
+++ src/CHL/NewTest/Main.php
@@ -7,7 +7,7 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 use CHL\NewTest\Commands;
 
-class Main extends PluginBase
+class NewTest extends PluginBase
 {
     public function onEnable(): void
     {
